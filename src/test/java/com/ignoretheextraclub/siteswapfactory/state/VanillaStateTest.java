30eb61f66cc23dbf3a84f1acbfa229adf307fbe9
FunkyBookends <nonclercqc@gmail.com>
2017-01-15 14:28:57 +0000

    add test file

---
+++ src/test/java/com/ignoretheextraclub/siteswapfactory/state/VanillaStateTest.java
@@ -0,0 +1,103 @@
+package com.ignoretheextraclub.siteswapfactory.state;
+
+import com.ignoretheextraclub.siteswapfactory.thros.VanillaThrow;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by caspar on 15/01/17.
+ */
+public class VanillaStateTest
+{
+    @Test
+    public void canThrow() throws Exception
+    {
+
+    }
+
+    @Test
+    public void getAvailableThrows() throws Exception
+    {
+
+    }
+
+    @Test
+    public void getMaxThrow() throws Exception
+    {
+
+    }
+
+    @Test
+    public void getNumObjects() throws Exception
+    {
+
+    }
+
+    @Test
+    public void isGroundState() throws Exception
+    {
+
+    }
+
+    @Test
+    public void getThrow() throws Exception
+    {
+
+    }
+
+    @Test
+    public void excitedness() throws Exception
+    {
+
+    }
+
+    @Test
+    public void getGroundState() throws Exception
+    {
+
+    }
+
+    @Test
+    public void testToString() throws Exception
+    {
+
+    }
+
+    @Test
+    public void equals() throws Exception
+    {
+
+    }
+
+    @Test
+    public void equals1() throws Exception
+    {
+
+    }
+
+    @Test
+    public void thro() throws Exception
+    {
+
+    }
+
+    @Test
+    public void getNextStates() throws Exception
+    {
+
+    }
+
+    @Test
+    public void canTransition() throws Exception
+    {
+
+    }
+
+    @Test
+    public void getFirstState() throws Exception
+    {
+
+    }
+
+}
